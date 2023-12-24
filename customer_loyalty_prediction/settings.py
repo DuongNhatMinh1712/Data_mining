bbd9936cc2986cf87f75d743770b9706318b85a5
Nhân Nguyễn Trọng <19521940@ms.uit.edu.vn>
2023-12-25 04:43:59 +0700

    Edit Frontend

--- customer_loyalty_prediction/settings.py
+++ customer_loyalty_prediction/settings.py
@@ -125,3 +125,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/']
--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
-# Create your views here.
+@csrf_exempt
 def show_result(request):
-    return "<h1> X </h1>"
+    if request.method == 'POST':
+        # Assuming requestData is passed as a query parameter
+        requestData = request.GET.dict()
+        print(requestData)
+        # Perform your processing on the requestData here
 
+        # For example, create a result dictionary
+        result = {'status': 'success', 'message': 'Processing successful', 'data': requestData}
+
+        # Return the result as JSON
+        return HttpResponse(result)
+    else:
+        # Handle other HTTP methods if needed
+        result = {'status': 'error', 'message': 'Invalid request method'}
+        return HttpResponse(result)
+@csrf_exempt
 class HomeView(View):
     def get(seft, request):
         return render(request, 'homepage/forms.html')
